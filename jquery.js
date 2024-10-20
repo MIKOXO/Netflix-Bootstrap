08bb1c59c128f0ad80ad4806f1e1dfbd3cc1909b
Mikiyas Nigatu <mikermount19@gmail.com>
2024-10-20 22:30:11 +0300

    first commit

---
+++ jquery.js
@@ -0,0 +1,47 @@
+$(".message1").hide();
+
+$(document).ready(function () {
+  $(".faq1").click(function () {
+    $(".message1").slideToggle();
+  });
+});
+
+$(".message2").hide();
+
+$(document).ready(function () {
+  $(".faq2").click(function () {
+    $(".message2").slideToggle();
+  });
+});
+
+$(".message3").hide();
+
+$(document).ready(function () {
+  $(".faq3").click(function () {
+    $(".message3").slideToggle();
+  });
+});
+
+$(".message4").hide();
+
+$(document).ready(function () {
+  $(".faq4").click(function () {
+    $(".message4").slideToggle();
+  });
+});
+
+$(".message5").hide();
+
+$(document).ready(function () {
+  $(".faq5").click(function () {
+    $(".message5").slideToggle();
+  });
+});
+
+$(".message6").hide();
+
+$(document).ready(function () {
+  $(".faq6").click(function () {
+    $(".message6").slideToggle();
+  });
+});
